794c037380e1737c645b515f117350056647303f
tkwbr999 <tkwbr999@gmail.com>
2025-03-23 17:31:09 +0900

    feat: add atoms for managing node and edge updates in mind-map-store

--- src/store/mind-map-store.ts
+++ src/store/mind-map-store.ts
@@ -1,6 +1,12 @@
 import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import {
+    addEdge,
+    applyEdgeChanges,
+    applyNodeChanges,
+  Connection,
+  EdgeChange,
+  NodeChange,
   type Edge,
   type Node,
 } from "@xyflow/react";
@@ -34,3 +40,27 @@ export const edgesAtom = atomWithStorage<Edge[]>("mindmap-edges", initialEdges);
 // 選択中のノードを管理するアトム
 export const selectedNodeAtom = atom<Node | null>(null);
 
+
+// ノードを更新するアトム
+export const nodesChangeAtom = atom(null, (get, set, changes: NodeChange[]) => {
+    set(nodesAtom, applyNodeChanges(changes, get(nodesAtom)));
+  });
+  
+  // エッジを更新するアトム
+  export const edgesChangeAtom = atom(null, (get, set, changes: EdgeChange[]) => {
+    set(edgesAtom, applyEdgeChanges(changes, get(edgesAtom)));
+  });
+  
+  // ノード接続のアトム
+  export const connectAtom = atom(null, (_, set, connection: Connection) => {
+    set(edgesAtom, (eds) =>
+      addEdge(
+        {
+          ...connection,
+          animated: true,
+          style: { stroke: "#f6b93b", strokeWidth: 3 },
+        },
+        eds
+      )
+    );
+  });
